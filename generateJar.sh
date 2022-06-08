418cf59896fa5e05f154b7aca76facab53cfbaae
Miguel Castro <miguel.c@edu.pucrs.br>
2022-06-08 23:41:03 +0000

    rodando

---
+++ generateJar.sh
@@ -0,0 +1,24 @@
+cd MSP6_api-gateway/
+mvn clean package
+
+cd ..
+
+cd MSP6_naming-server/
+mvn clean package
+
+cd ..
+
+cd MSP6_currency-conversion-service/
+mvn clean package
+
+cd ..
+
+cd MSP6_currency-exchange-service/
+mvn clean package
+
+cd ..
+
+cd MSP6_CotationHistory/
+mvn clean package
+
+cd ..
